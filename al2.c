c47fb8dc04d2f8d483008e7c23380b662c1a6831
douglas565 <138054959+douglas565@users.noreply.github.com>
2023-08-23 13:51:46 -0300

    Create al2.c

---
+++ al2.c
@@ -0,0 +1,24 @@
+// l123b - e2
+// programa multiplicação
+// douglas ramos 
+//2023/08/22
+
+#include <stdio.h>
+
+int main()
+{
+int n1;
+int n2;
+int result;
+
+printf("Digite um valor:\n");
+scanf("%d",&n1);
+printf("Digite outro valor:\n");
+scanf("%d",&n2);
+
+result = (n1*n2);
+
+printf ("%d multiplicado por %d vale %d.\n", n1,n2,result);
+
+return 0;
+}
