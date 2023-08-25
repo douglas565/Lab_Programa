fb66fd1dcb11e2f6855973d46e1236cd0a60aa9b
douglas565 <138054959+douglas565@users.noreply.github.com>
2023-08-25 16:01:08 -0300

    Add files via upload

---
+++ exer.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+ int n1;
+ int n2;
+ int rest;
+
+ scanf("%d",&n1);
+ scanf("%d",&n2);
+
+if(n1>n2){
+printf("%d é maior que %d",n1,n2);
+}
+
+ if(n1<n2){
+printf("%d é maior que %d",n2,n1);
+}
+
+}
